3aa0262904f03bbca7ef004dd755545bb07d99a5
Ayush Sarvaiya <ayush.sarvaiya@matrixcomsec.com>
2026-09-25 10:45:49 +0000

    docs: Some changes

--- tests/OrderApiTests.cs
+++ tests/OrderApiTests.cs
@@ -37,7 +37,7 @@ public class OrderApiTests : IClassFixture<WebApplicationFactory<Program>>
     {
         var client = _factory.CreateClient();
 
-        var response = await client.GetAsync("/api/order/calculate-total-with-tax?price=100&quantity=2&discountPercent=10&taxPercent=15");
+        var response = await client.GetAsync("/api/order/total-with-tax?price=100&quantity=2&discountPercent=10&taxPercent=15");
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
